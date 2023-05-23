7773bbcd765af2c9a9e6861ee136ff68aa4f9759
liamfoster666 <liamfoster666@gmai.com>
2023-05-23 15:37:42 +0530

    New UI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const express = require("express"); 
+const express = require("express");
 const bodyParser = require("body-parser");
 const v1WorkoutRouter = require("./api/v1/routes/workoutRoutes.js");
 const v1PingRouter = require("./api/v1/routes/pingRouter.js");
@@ -6,12 +6,12 @@ const swaggerDocs = require('./api/v1/docs/swagger.js');
 const path = require("path");
 
 
-const app = express(); 
-const PORT = process.env.PORT || 10000; 
+const app = express();
+const PORT = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use("/api/v1/workouts", v1WorkoutRouter);
 app.use("/ping", v1PingRouter);
@@ -19,7 +19,7 @@ app.use("/ping", v1PingRouter);
 swaggerDocs(app, PORT);
 
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.log(`API is listening on port http://localhost:${PORT}`);
     // V1SwaggerDocs(app, PORT);
 });
